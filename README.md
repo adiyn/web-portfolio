bc36246d13b9e4bd801d0970d5684493738534c1
adiyn <adiyn@noreply.codeberg.org>
2026-09-23 10:39:27 +0200

    removed useless stuff added return button to shifumi

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # web-projects
 
+### this repo counts as my portfolio and serves also as a recollection of my small web projects 
--- shifumi/App.js
+++ shifumi/App.js
@@ -25,23 +25,6 @@ function getResult(player, robot) {
     return (player - robot + 3) % 3;
 }
 
-function play(player) {
-    const robot = randomMove();
-    const res = getResult(player, robot);
-
-    if (res === 0) {
-        state.nulls++;
-        result.textContent = "EGALITEEEEE";
-    } else if (res === 1) {
-        state.victories++;
-        result.textContent = "VICTOIRE DUUU JOUEUUUUUUR";
-    } else {
-        state.defeats++;
-        result.textContent = "DEFAITE TOTALE.";
-    }
-    show();
-}
-
 function show() {
     victories.textContent = state.victories;
     defeats.textContent = state.defeats;
@@ -52,18 +35,35 @@ function reset() {
     state.victories = 0;
     state.defeats = 0;
     state.nulls = 0;
-    result.textContent = "Bienvenue sur ShiFuMi :)";
+    result.textContent = "Welcome to ShiFuMi :)";
+    show();
+}
+
+function play(player) {
+    const robot = randomMove();
+    const res = getResult(player, robot);
+
+    console.log(`player chose ${player} and bot chose ${robot} result is ${res}`);
+    
+    if (res === 0) {
+        state.nulls++;
+        result.textContent = "Null";
+    } else if (res === 1) {
+        state.victories++;
+        result.textContent = "Player won";
+    } else {
+        state.defeats++;
+        result.textContent = "Bot won.";
+    }
     show();
 }
 
 playButtons.addEventListener("click", event => {
-    const btn = event.target.closest(".btn");
+    const btn = event.target;
     if (!btn) return;
 
     const player = moves[btn.dataset.move];
-    if (player !== undefined) {
-        play(player);
-    }
+    play(player);
 });
 
 resetButton.addEventListener("click", reset);
